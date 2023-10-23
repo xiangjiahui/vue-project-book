2313e0b7e019de99cfb940394d3b9af68eea3b4f
xiangjh <xiangjh@bokesoft.com>
2023-10-23 13:06:23 +0800

    添加注册页面的代码

--- src/router/index.js
+++ src/router/index.js
@@ -31,7 +31,8 @@ const routes = [
   { path: '/search', name: 'search', component: () => import('@/views/Search/Sear.vue') },
   { path: '/user', name: 'user', component: () => import('@/views/User/User.vue') },
   { path: '/list/:id', name: 'bookList', component: () => import ('@/views/List/BookList.vue'), props: true },
-  { path: '/login', name: 'login', component: Login }
+  { path: '/login', name: 'login', component: Login },
+  {path: '/register', name: 'register', component: () => import('@/views/Register/Register.vue') }
 ]
 
 const router = new VueRouter({
@@ -46,20 +47,20 @@ const router = new VueRouter({
 })
 
 //导航守卫
-router.beforeEach((to, from, next) => {
-  const token = localStorage.getItem('token');
-  if (to.path !== '/login') {
-    if (!token) {
-      Toast.fail({
-        message: '未登录',
-        duration: 1500,
-        onClose: () => {
-          next('/login');
-        }
-      });
-    }
-  }
-  next();
-});
+// router.beforeEach((to, from, next) => {
+//   const token = localStorage.getItem('token');
+//   if (to.path !== '/login') {
+//     if (!token) {
+//       Toast.fail({
+//         message: '未登录',
+//         duration: 1500,
+//         onClose: () => {
+//           next('/login');
+//         }
+//       });
+//     }
+//   }
+//   next();
+// });
 
 export default router
---
+++ src/views/Register/Register.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="register-container">
+    <van-form @submit="onSubmit" @failed="onFailed">
+      <van-field
+          v-model="username"
+          name="username"
+          label="用户名"
+          placeholder="用户名"
+          :rules="[{ pattern ,message: '用户名只能输入1-9位的大小写英文字母' }]"
+      />
+      <van-field
+          v-model="password"
+          type="password"
+          name="password"
+          label="密码"
+          placeholder="密码"
+          :rules="[{ validator, message: '密码错误' }]"
+      />
+      <div style="margin: 16px;">
+        <van-button round block type="info" native-type="submit">注册</van-button>
+      </div>
+    </van-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Register",
+  data() {
+    return {
+      username: '',
+      password: '',
+      pattern : /^[a-zA-Z]{1,9}$/
+    }
+  },
+  methods: {
+    onSubmit(values) {
+      console.log('submit', values);
+    },
+    onFailed(values) {
+      console.log('failed', values);
+    },
+    validator(val) {
+      return /^[a-zA-Z0-9]{6,16}$/.test(val);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
